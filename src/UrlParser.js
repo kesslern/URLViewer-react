538c4aac66cf09e18eee03a7eacee6f4950fda06
kesslern <nathant93@gmail.com>
2017-12-07 22:55:39 -0500

    Parse and reconstruct entire URL with regex

--- src/UrlParser.js
+++ src/UrlParser.js
@@ -1,36 +1,30 @@
 const regex = {
-  host: /\S+(?=\?|^)/,
-  queryString: /(\?\S+)(?:#\S)?$/
+  url: /([a-zA-Z]+:\/\/)?([a-zA-Z0-9.-]+)?(\/[/a-zA-Z0-9]+)?(\?[a-zA-Z0-9&=%]+)?(#[a-zA-Z0-9]+)?/
 }
 
-function parseQueryParams (urlString) {
-  const regexResult = regex.queryString.exec(urlString)
-  if (regexResult) {
-    return regexResult[0]
-      .substr(1)
-      .split('&')
-      .map(x => (x.split('=')))
-      .map(([x, y]) => [decodeURIComponent(x), decodeURIComponent(y || '')])
-  } else {
-    return undefined
-  }
-}
-
-function parseHost (urlString) {
-  const regexResult = regex.host.exec(urlString)
-  return regexResult ? regexResult[0] : ''
+function parseQueryParamString (queryParams) {
+  return queryParams
+    .substr(1)
+    .split('&')
+    .map(x => (x.split('=')))
+    .map(([x, y]) => [decodeURIComponent(x), decodeURIComponent(y || '')])
 }
 
 class UrlParser {
   static parseUrl (urlString) {
+    const parsedUrl = regex.url.exec(urlString)
     return {
-      host: parseHost(urlString),
-      queryParams: parseQueryParams(urlString)
+      protocol: parsedUrl[1],
+      host: parsedUrl[2],
+      path: parsedUrl[3],
+      queryParams: parseQueryParamString(parsedUrl[4] || '?'),
+      anchor: parsedUrl[5]
     }
   }
 
   static toString (parsedUrl) {
-    return parsedUrl.host + '?' + parsedUrl.queryParams.map(it => it[0] + '=' + it[1]).join('&')
+    return parsedUrl.protocol +
+      parsedUrl.host + parsedUrl.path + parsedUrl.queryParams.map(it => it[0] + '=' + it[1]).join('&') + parsedUrl.anchor
   }
 }
 
