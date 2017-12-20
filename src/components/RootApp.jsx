6fa23c52d4dc3595f9b6e6f11061285507af15c0
Nathan Kessler <nathant93@gmail.com>
2017-12-19 22:57:11 -0500

    Only render edit area when url not empty

--- src/components/RootApp.jsx
+++ src/components/RootApp.jsx
@@ -5,33 +5,38 @@ import QueryParam from './QueryParam'
 import { connect } from 'react-redux'
 
 const mapStateToProps = (state) => ({
-  queryParams: state.queryParams
+  queryParams: state.queryParams,
+  url: state.url
 })
 
 class RootApp extends React.Component {
   static propTypes = {
-    queryParams: PropTypes.array
+    queryParams: PropTypes.array,
+    url: PropTypes.string
   }
 
   render () {
+    const editArea = this.props.url
+      ? <div>
+        {!this.props.queryParams
+          ? 'No query params'
+          : <ul id='query-params'>
+            {this.props.queryParams.map((param, index) => (
+              <QueryParam
+                key={index}
+                index={index}
+              />
+            ))}
+          </ul>
+        }
+      </div> : null
+
     return (
       <div id='app' className='container'>
         <div>
           <URLInput/>
         </div>
-        <div>
-          {!this.props.queryParams
-            ? 'No query params'
-            : <ul id='query-params'>
-              {this.props.queryParams.map((param, index) => (
-                <QueryParam
-                  key={index}
-                  index={index}
-                />
-              ))}
-            </ul>
-          }
-        </div>
+        {editArea}
       </div>
     )
   }
